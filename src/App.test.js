8a9e99a3ad7ff9c8da2ac5bc89886d882d5dee94
PaulBoutot <paulvboutot@hotmail.com>
2024-02-20 13:31:17 -0500

    added sample code to test coverage report

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { add, subtract } from './FooBar';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('adding two numbers works', () => {
+  const n = add(1, 2);
+  expect(n).toBe(3);
+});
+
+test('subtracting two numbers works', () => {
+  const n = subtract(2, 1);
+  expect(n).toBe(1);
 });
