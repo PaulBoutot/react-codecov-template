712f309ff8100e7474a22972a7f5f807325dbb09
PaulBoutot <paulvboutot@hotmail.com>
2024-02-26 13:46:04 -0500

    added tests

--- src/Foo.test.js
+++ src/Foo.test.js
@@ -8,9 +8,11 @@ describe('Foo Tests', () => {
 
   it('should add two numbers correctly', () => {
     expect(add(2, 4)).toBe(6);
+    expect(add(4, 4)).toBe(8);
   });
 
   it('should subtract two numbers correctly', () => {
     expect(subtract(4,2)).toBe(2);
+    expect(subtract(5,2)).toBe(3);
   });
 });
